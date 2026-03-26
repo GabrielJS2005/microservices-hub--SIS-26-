dfcebf43f38d9208aea4d9dc581391f2e599dcd0
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-25 21:55:03 -0300

    feat: add entidadde, carga do DB

---
+++ ms-pedidos/src/main/java/com/github/GabrielJS2005/ms_pedidos/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.github.GabrielJS2005.ms_pedidos.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
